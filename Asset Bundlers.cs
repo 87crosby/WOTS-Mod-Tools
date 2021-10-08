29606376336644aabb3667328836209941baf99d
Connor Howe <71791452+87crosby@users.noreply.github.com>
2021-10-08 17:43:48 -0400

    Add files via upload

---
+++ Asset Bundlers.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class CreateAssetBundles
+{
+	[MenuItem("Assets/Build AssetBundles")]
+	static void BuildAllAssetBundles()
+	{
+		string assetBundleDirectory = "Assets/AssetBundles";
+		if(!System.IO.Directory.Exists(assetBundleDirectory))
+		{
+			System.IO.Directory.CreateDirectory(assetBundleDirectory);
+		}
+		BuildPipeline.BuildAssetBundles (assetBundleDirectory, BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.StandaloneWindows);
+	}
+}
